e3f40e4c5e8c41cb9228bbd2b83fd82597f48a5a
Hilde-otieno <meghanadhiambootieno@gmail.com>
2024-06-08 20:01:49 +0300

    made changes

--- calendar.js
+++ calendar.js
@@ -1,26 +1,19 @@
-k = `OejPuggsH64UhacdrflwolmIBHpq6uto`;
-
+k = `F4hSTt7qTMQn4rvfIFonQXYlcBC37Sdq`;
 const userCountry = document.getElementById('country-input');
 const userYear = document.getElementById('year-input');
 const userMonth = document.getElementById('month-input');
 const userDay = document.getElementById('day-input');
-
 let today = new Date();
 userYear.value = today.getFullYear();
-
 const $cardContainer = $('#card-container');
-
 let cardCount = 0;
 let display = false;
 let countryList = [];
 let holidayList = [];
-
 handleGetCountries();
-
 $('#today-button').on('click', useToday);
 $('#reset-button').on('click', clearHolidays);
 $('form').on('submit', handleGetHolidays);
-
 // API call for country list
 function handleGetCountries() {
     $.ajax({
@@ -35,11 +28,9 @@ function handleGetCountries() {
         }
     );
 }
-
 function countryDropdown() {
     let fragment = document.createDocumentFragment();
     countryList.forEach(country => {
-
         let opt = document.createElement('option');
         opt.innerHTML = country.country_name;
         opt.value = country['iso-3166'];
@@ -48,16 +39,13 @@ function countryDropdown() {
     });
     userCountry.appendChild(fragment);
 }
-
-
+// API call for holidays
 function handleGetHolidays(evt){
     evt.preventDefault();
-
     let leapYear = false;
     if ((0 == userYear.value % 4) && (0 != userYear.value % 100) || (0 == userYear.value % 400)){
         leapYear = true;
     }
-
     if (userCountry.value === 'Please Choose a Country') {
         alert('You must choose a country first!')
     } else if (!userYear.value) {
@@ -87,17 +75,14 @@ function handleGetHolidays(evt){
         );
     }
 }
-
 function renderHoliday(){
     if (holidayList.length > 0) {
         holidayList.forEach(holiday => {
             createCard();
-
             let $cardDate = $(`#card-date-${cardCount}`);
             let $cardType = $(`#card-type-${cardCount}`);
             let $cardName = $(`#card-name-${cardCount}`);
             let $cardDesc = $(`#card-desc-${cardCount}`);
-
             $cardDate.text(holiday.date.iso.slice(0,10));
             $cardType.text(holiday.primary_type)
             $cardName.text(holiday.name);
@@ -105,14 +90,11 @@ function renderHoliday(){
         });
     } else {
         createCard();
-
         let $cardDate = $(`#card-date-${cardCount}`);
         let $cardType = $(`#card-type-${cardCount}`);
         let $cardName = $(`#card-name-${cardCount}`);
         let $cardDesc = $(`#card-desc-${cardCount}`);
-
         let countryName = nameFromIso(userCountry.value);
-
         $cardDate.text(`${userYear.value}-${userMonth.value}-${userDay.value}`);
         $cardType.text(countryName)
         $cardName.text('None');
@@ -120,43 +102,33 @@ function renderHoliday(){
     }
     display = true;
 }
-
 function createCard() {
     if (display) {
         removeCards();
     }
-
     cardCount++;
     let newCard = document.createElement('section');
-
     newCard.setAttribute('id',`card-${cardCount}`);
     newCard.innerHTML = `<span id='card-date-${cardCount}'></span><br><br><span id='card-type-${cardCount}'></span><h3 id='card-name-${cardCount}'>New Card</h3>
     <p id='card-desc-${cardCount}'>New Desc</p>`;
-
     $cardContainer.append(newCard);
 }
-
 function removeCards() {
     $cardContainer.empty();
     cardCount = 0;
     display = false;
 }
-
 function clearHolidays(evt) {
     evt.preventDefault();
-
     removeCards();
 }
-
 function nameFromIso(iso) {
     let countryName = countryList.find(country => country['iso-3166'] === iso)
     return countryName.country_name
 }
-
 function useToday(evt) {
     evt.preventDefault();
     userMonth.value = today.getMonth() + 1
     userDay.value = today.getDate()
-
     userMonth.classList.remove('first-choice');
 }
